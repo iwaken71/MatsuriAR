cd5e56040326a518bf3fffc0090d49efcf4ba961
iwaken71 <>
2020-08-30 21:34:29 +0900

    add JapanMaterials

---
+++ Assets/Externals/JapanMaterials/lampion/readme.txt
@@ -0,0 +1,15 @@
+*****************************
+***   Japanese Lampions   ***
+*****************************
+by The Topicbird
+www.thetopicbird.com
+
+
+INSTRUCTIONS:
+Simply drag any of the lampion Prefabs located in the "prefabs" folder into your Scene. You can adjust their flickering behaviour by altering the "LightFlicker" script's sliders located on the Prefab. You can also overwrite the light's settings at runtime by accessing the public method "overrideSettings" of the "LightFlicker" script.
+
+To turn the lights off entirely, simply uncheck the "Flickering" value on the "LightFlicker" script and set "Brightness" to 0.
+
+
+For any feedback or questions please contact us at talk@thetopicbird.com.
+If you like this bundle, feel free to leave a comment in the Assetstore. We really appreciate it.
---
+++ Assets/Externals/JapanMaterials/lampion/scripts/LightFlicker.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightFlicker : MonoBehaviour {
+
+	[SerializeField] bool flickering = true;
+	[SerializeField] [Range (0f, 2f)] float brightness = 1f;
+	[SerializeField] [Range (0f, 0.2f)] float speed = 0.035f;
+	[SerializeField] [Range (0f, 0.2f)] float amplitude = 0.35f;
+
+	float currentBrightness;
+	Material mat;
+
+	// Use this for initialization
+	void Start () {
+		mat = this.GetComponent<MeshRenderer> ().materials[0];
+
+		reset ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (!flickering) {
+			return;
+		}
+
+		// flickering behaviour
+		currentBrightness += Random.Range (-speed, speed);
+		currentBrightness = Mathf.Clamp (currentBrightness, Mathf.Clamp(brightness - amplitude, 0f, brightness), brightness + amplitude);
+
+		// updating the material with the current flickering
+		mat.SetColor ("_EmissionColor", Color.white * currentBrightness);
+	}
+
+	void reset(){
+		currentBrightness = brightness;
+
+		if (!flickering) {
+			mat.SetColor ("_EmissionColor", Color.white * brightness);
+		}
+	}
+
+	// for access from other scripts
+	public void overrideSettings(bool _flickering, float _brightness, float _speed, float _amplitude){
+		flickering = _flickering;
+		brightness = _brightness;
+		speed = _speed;
+		amplitude = _amplitude;
+
+		reset ();
+	}
+}
