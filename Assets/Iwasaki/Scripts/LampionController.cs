ea4a606ecb2b9dd07bc33621e4603b7759f95146
iwaken71 <>
2020-09-02 05:42:55 +0900

    edit Scene

---
+++ Assets/Iwasaki/Scripts/LampionController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+namespace Iwaken
+{
+    public class LampionController : MonoBehaviour
+    {
+        [SerializeField] LampionMaterial[] lampionMaterials;
+        [SerializeField] MeshRenderer meshRenderer;
+
+        public void ChangeColor(LampionColor color)
+        {
+            meshRenderer.material = lampionMaterials.Where(l => color == l.color).Select(l => l.material).First();
+        }
+        void Update()
+        {
+            if (Input.GetKeyDown("1"))
+            {
+                ChangeColor(LampionColor.Gray);
+
+            }
+            if (Input.GetKeyDown("2"))
+            {
+                Debug.Log("2");
+                ChangeColor(LampionColor.Red);
+            }
+            if (Input.GetKeyDown("3"))
+            {
+                ChangeColor(LampionColor.Blue);
+            }
+            if (Input.GetKeyDown("4"))
+            {
+                ChangeColor(LampionColor.Green);
+            }
+        }
+    }
+    [System.Serializable]
+    public class LampionMaterial
+    {
+        public LampionColor color;
+        public Material material;
+    }
+    [System.Serializable]
+    public enum LampionColor
+    {
+        Gray = 0,
+        Red = 1,
+        Blue = 2,
+        Green = 3
+    }
+}
---
+++ Assets/Iwasaki/Scripts/RayManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+namespace Iwaken
+{
+    [RequireComponent(typeof(ARRaycastManager))]
+    public class RayManager : MonoBehaviour
+    {
+
+        ARRaycastManager raycastManager;
+        List<ARRaycastHit> hitResults = new List<ARRaycastHit>();
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+                // レイと検出平面が衝突時
+                if (Physics.Raycast(ray, out hit))
+                {
+                    if (hit.collider.GetComponent<LampionController>())
+                    {
+                        hit.collider.GetComponent<LampionController>().ChangeColor(LampionColor.Red);
+                    }
+                }
+            }
+        }
+    }
+}
